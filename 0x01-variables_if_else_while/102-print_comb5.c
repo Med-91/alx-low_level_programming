4166f150895762b181cdd0f651bc084cb5e9e04e
Med-91 <mohammed.benjana@hotmail.com>
2023-03-17 18:09:26 -0700

    Task_12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ * main - main fonction
+ * Return: always 0
+ * combination ( ij km)
+ */
+int main(void)
+{
+	int i, j, k, m;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+		for (k = 0; k < 10; k++)
+		{
+
+			for (m = 0; m < 10; m++)
+			{
+			if ((m > j) && (k >= i))
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(' ');
+				putchar(k + '0');
+				putchar(m + '0');
+				if ((i != 9) || (j != 8) || (k != 9) || (m != 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			}
+		}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
