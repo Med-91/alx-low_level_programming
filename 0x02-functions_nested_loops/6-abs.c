81ce1c426b5ec781e10b4009e4c738cda38da03e
Med-91 <mohammed.benjana@hotmail.com>
2023-03-21 15:55:06 -0700

    Task_6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - fonction entry point
+ * @n: input integer
+ * Return: absolute value of an integer.
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
+
