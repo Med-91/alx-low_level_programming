0fc092da398d6c54052ce49a35e421abd18b54e6
Med-91 <mohammed.benjana@hotmail.com>
2023-03-21 17:23:14 -0700

    Tas_ 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - fonction entry point
+ * @a: input integer
+ * @b: input integer
+ * Return: addition of tow  integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
