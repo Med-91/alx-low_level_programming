06e4bb7846a589902658520caf3bca6b26763738
Med-91 <mohammed.benjana@hotmail.com>
2023-03-21 18:17:11 -0700

    Task_11-modif-2

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,15 +10,15 @@ void print_to_98(int n)
 
 	if (n <= 98)
 	{
-		for (i = n; i <= 98; i++)
+		for (i = n; i < 98; i++)
 			printf("%d, ", i);
-		printf("\n");
+		printf("98\n");
 	}
 	else
 	{
-		for (i = n; n >= 98; i--)
+		for (i = n; n > 98; i--)
 			printf("%d, ", i);
-		printf("\n");
+		printf("98\n");
 	}
 }
 
