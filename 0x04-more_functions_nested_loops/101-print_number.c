63d3c78f4b1bdded282507db6319a0830d7e1889
Med-91 <mohammed.benjana@hotmail.com>
2023-03-23 19:36:12 -0700

    task_12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,60 @@
+#include "main.h"
+
+/**
+ * print_number - function that prints an integer.
+ * _putchar: prints character
+ * @n: input integer
+ */
+
+void print_number(int n)
+{
+	if (n >= 0 && n < 10)
+	{
+		_putchar(n + '0');
+	}
+	else if (n >= 10 && n < 100)
+	{
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else if (n >= 100 && n < 1000)
+	{
+		_putchar(n / 100 + '0');
+		_putchar((n % 100) / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else if (n >= 1000 && n < 10000)
+	{
+		_putchar(n / 1000 + '0');
+		_putchar((n % 1000) / 100 + '0');
+		_putchar(((n % 100) / 10) / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else if (n < 0 && n > -10)
+	{
+		_putchar('-');
+		_putchar(-n + '0');
+	}
+	else if (n <= -10 && n > -100)
+	{
+		_putchar('-');
+		_putchar(-n / 10 + '0');
+		_putchar(-n % 10 + '0');
+	}
+	else if (n <= -100 && n > -1000)
+	{
+		_putchar('-');
+		_putchar(-n / 100 + '0');
+		_putchar((-n % 100) / 10 + '0');
+		_putchar(-n % 10 + '0');
+	}
+	else if (n <= -1000 && n > -10000)
+	{
+		_putchar('-');
+		_putchar(-n / 1000 + '0');
+		_putchar((-n % 1000) / 100 + '0');
+		_putchar(((-n % 100) / 10) / 10 + '0');
+		_putchar(-n % 10 + '0');
+	}
+}
+
