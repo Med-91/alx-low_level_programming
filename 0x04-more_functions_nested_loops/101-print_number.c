ebeb566d31b9d98d12397b2239e74a8b574e14eb
Med-91 <mohammed.benjana@hotmail.com>
2023-03-24 17:32:52 -0700

    task_12-modif-7

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,19 +7,32 @@
  */
 void print_number(int n)
 {
-	unsigned long int a;
-
 	if (n < 0)
 	{
 		_putchar('-');
-		a = -n;
+		n = -n;
 	}
-	else
-		a = n;
-	if (a / 10 != 0)
+	if (n >= 0 && n < 10)
 	{
-		print_number(a / 10);
+		_putchar(n + '0');
 	}
-	_putchar(a % 10 + '0');
+	else if (n >= 10 && n < 100)
+	{
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else if (n >= 100 && n < 1000)
+	{
+		_putchar(n / 100 + '0');
+		_putchar((n % 100) / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else if (n >= 1000 && n < 10000)
+	{
+		_putchar(n / 1000 + '0');
+		_putchar((n % 1000) / 100 + '0');
+		_putchar((n % 100) / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
 }
 
