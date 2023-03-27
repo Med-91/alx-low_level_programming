910c30b0134abf43f52b9f1ce958121dd0e51bbc
Med-91 <mohammed.benjana@hotmail.com>
2023-03-27 15:24:35 -0700

    task_3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - function that prints a string, followed by a new line, to stdout
+ * _putchar: prints character
+ * @str: input pointer
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+}
+
