8dae7168bf0fbeb04b2ee15cb2098435ad94d06c
Med-91 <mohammed.benjana@hotmail.com>
2023-03-28 16:49:44 -0700

    task_11-modif-4

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,17 +9,16 @@
 
 int main(void)
 {
-	int n = 0, key = 0;
+	int n = 0, cum = 0;
 
 	srand(time(0));
-	while ((n + key + '0') < 2772)
+	while (cum < 2772)
 	{
 		n = rand() % 91;
-		key += n + '0';
+		cum += n + '0';
 		printf("%c", n + '0');
 	}
-	n = 2772 - key - '0';
-	printf("%c", n + '0');
+	n = 2772 - (cum - n);
+	printf("%c", n);
 	return (0);
 }
-
