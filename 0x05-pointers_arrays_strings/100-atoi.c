83c2719f83e239587d38a3da00dd19b3b693410d
Med-91 <mohammed.benjana@hotmail.com>
2023-03-27 19:52:25 -0700

    task_100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _atoi - function that convert a string to an integer.
+ * @s: input string
+ * Return: return an integer
+ */
+
+int _atoi(char *s)
+{
+	int val = 0, i = 0;
+	int dig = 0, sign = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] < 57 &&s[i])
+		{
+			val = val * 10 + (-1 ^ sign) * s[i];
+			sign = 0;
+			dig = 1;
+		}
+		else if (s[i] == '-')
+			sign++;
+		i++;
+		if (dig == 1 && s[i] != [ : digit : ])
+			return (val);
+	}
+	if (s[i] == '0' && dig == 0)
+		return (0);
+}
+
