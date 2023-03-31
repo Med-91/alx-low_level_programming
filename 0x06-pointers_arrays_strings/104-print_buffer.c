1b24bfcc66585c7b396b6ddfcfd9a74fd0cbf4ac
Med-91 <mohammed.benjana@hotmail.com>
2023-03-30 19:25:12 -0700

    task_12

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - prints the content of size bytes of the buffer.
+ * @b: input pointer to buffer
+ * @size: input buffer size
+ */
+
+void print_buffer(char *b, int size)
+{
+	int i = 0, j, k;
+
+	if (size <= 0)
+		printf("\n");
+	else
+		while (i < size)
+		{
+			printf("%x: ", i);
+			for (k = 0; k < 10; k += 2)
+			{
+				if (i + k < size - 1)
+				printf("%x%x ", b[i + k], b[i + k + 1]);
+				else
+					printf(" ");
+			}
+			for (j = 0; j < 10; j++)
+			{
+				if (i + j == size - 1)
+					break;
+				if (b[i + j] < 32 || b[i + j] == 127)
+					printf(".");
+				else
+					printf("%c", b[i + j]);
+			}
+			printf("\n");
+			i += 10;
+		}
+}
+
