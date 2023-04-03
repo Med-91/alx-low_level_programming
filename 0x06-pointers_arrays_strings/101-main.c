a61919da539e1416fc1e74aa9466f6fcf7a211a9
Med-91 <mohammed.benjana@hotmail.com>
2023-04-03 08:27:07 -0700

    Executable

---
+++ 0x06-pointers_arrays_strings/101-main.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+#include <math.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_number(98);
+    _putchar('\n');
+    print_number(402);
+    _putchar('\n');
+    print_number(1024);
+    _putchar('\n');
+    print_number(0);
+    _putchar('\n');
+    print_number(pow(-2,31));
+    _putchar('\n');
+    _putchar('\n');
+    _putchar(-(-12 % 10) + '0');
+    return (0);
+
+}
+
