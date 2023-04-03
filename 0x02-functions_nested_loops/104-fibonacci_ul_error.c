4b3c7d322a1e783e59a0141a1ebe8b34631666db
Med-91 <mohammed.benjana@hotmail.com>
2023-04-03 11:03:36 -0700

    main

---
+++ 0x02-functions_nested_loops/104-fibonacci_ul_error.c
@@ -0,0 +1,26 @@
+/*this program gives false output because of the overflow of the type unsigned long */
+
+#include <stdio.h>
+/**
+ * main - fonction entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	int  i;
+	unsigned long num, a = 1, b = 2;
+
+	printf("1, 2, ");
+	for (i = 0; i < 96; i++)
+	{
+		num = a + b;
+		a = b;
+		b = num;
+		printf("%lu", num);
+		if (i != 95)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
+
