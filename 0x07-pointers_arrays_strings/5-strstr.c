43f12e3f451aa1d847076ff002e3e20ecd902abc
Med-91 <mohammed.benjana@hotmail.com>
2023-04-03 17:00:55 -0700

    task_5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strstr - function that locates a substring.
+ * @haystack: input string
+ * @needle: input substring
+ * Return: the pointer of the first occurence
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, boul;
+
+	i = j = boul = 0;
+	while (haystack[i])
+	{
+		if (needle[j] == haystack[i])
+		{
+			boul = 1;
+			j++;
+		}
+		else
+		{
+			j = 0;
+			boul = 0;
+		}
+		if (!needle[j] && boul)
+			return (haystack + i - j + 1);
+		i++;
+	}
+
+	return (0);
+}
+
