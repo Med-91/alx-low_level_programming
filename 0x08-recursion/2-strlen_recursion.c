7ec7f929b855d00b72d815a41ec17e58ef0ff3a5
Med-91 <mohammed.benjana@hotmail.com>
2023-04-05 09:49:06 -0700

    task_2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion function that returns the length of a string.
+ * @s: inpu tstring
+ * Return: lenght of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		unsigned int len = 0;
+		len = _strlen_recursion(s + 1) + 1;
+		return (len);
+	}
+	else
+		return (0);
+}
+
