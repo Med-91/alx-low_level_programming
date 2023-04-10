b7072718be5cebcc7a0c16025671abacdde7ab25
Med-91 <mohammed.benjana@hotmail.com>
2023-04-10 06:54:12 -0700

    task_3-modif-1

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -26,3 +26,42 @@ int main(int argc, char **argv)
 	return (0);
 }
 
+
+/**
+ * _atoi - function that convert a string to an integer.
+ * @s: input string
+ * Return: return an integer
+ */
+
+int _atoi(char *s)
+{
+	int num, val = 0, i = 0;
+	int dig = 0, sign = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] <= 57 && s[i] >= 48)
+		{
+			if (sign % 2)
+			{
+				num = -(s[i] - '0');
+				val = val * 10 + num;
+			}
+			else
+			{
+				num = s[i] - '0';
+				val = val * 10 + num;
+			}
+			dig = 1;
+			if (s[i + 1] > 57 || s[i + 1] < 48)
+				break;
+		}
+		else if (dig == 0 && s[i] == '-')
+			sign++;
+		i++;
+	}
+	if (s[i] == '\0' && dig == 0)
+		return (0);
+	return (val);
+}
+
