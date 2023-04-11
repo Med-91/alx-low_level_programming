78711259ec4a7bc5cb7b6c43810a2fc862903f12
Med-91 <mohammed.benjana@hotmail.com>
2023-04-10 18:51:42 -0700

    task_5-modif-2

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -14,12 +14,12 @@ int main(int argc, char **argv)
 	int i = 0, cun = 0, cent;
 	int tab[5] = {25, 10, 5, 2, 1};
 
-	cent = _atoi(argv[1]);
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	cent = _atoi(argv[1]);
 	if (cent <= 0)
 	{
 		printf("0\n");
