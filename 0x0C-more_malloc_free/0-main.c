e4dec5aaef0f716073c572030315d534b087bac5
Med-91 <mohammed.benjana@hotmail.com>
2023-04-13 15:59:45 -0700

    mains

--- 0x0C-more_malloc_free/0-main.c
+++ 0x0C-more_malloc_free/0-main.c
@@ -22,6 +22,7 @@ int main(void)
     f = malloc_checked(sizeof(float) * 100000000);
     printf("%p\n", (void *)f);
     d = malloc_checked(INT_MAX);
+    printf("%d", INT_MAX);
     printf("%p\n", (void *)d);
     free(c);
     free(i);
