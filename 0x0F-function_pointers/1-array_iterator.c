114fe7710f0734266aa527ce3c714883a6195923
Med-91 <mohammed.benjana@hotmail.com>
2023-04-21 15:15:35 -0700

    task_1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * array_iterator - prints array's elements according to a function
+ * @array: input array
+ * @size: size of the array
+ * @action: pointer to a function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (action)
+		for (i = 0; i < (int)size; i++)
+			action(array[i]);
+}
+
