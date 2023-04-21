83fb814767e9a77509282b9ac3773d4befef6227
Med-91 <mohammed.benjana@hotmail.com>
2023-04-21 14:35:48 -0700

    task_0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name.
+ * @name: input name
+ * @f: input pointer to function
+ *
+ */
+
+void print_name(char *name, void(*f)(char *))
+{
+	f(name);
+}
+
