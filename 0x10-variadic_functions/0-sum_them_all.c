664aa84839f5554b293db31a1c9a0bb179ba52c4
Med-91 <mohammed.benjana@hotmail.com>
2023-04-20 17:31:16 -0700

    task_0-modif-1

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,7 +13,7 @@ int sum_them_all(const unsigned int n, ...)
 	int sum = 0, i;
 
 	if (n == 0)
-		return(0);
+		return (0);
 	va_start(ap, n);
 	for (i = 0; i < (int)n; i++)
 		sum = sum + va_arg(ap, int);
