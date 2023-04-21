b1754b2c1b4a6f2e4a9837c5fbc53a1175068c44
Med-91 <mohammed.benjana@hotmail.com>
2023-04-21 15:20:37 -0700

    task_0-modif-3

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-
+#include <stdlib.h>
 /**
  * print_name - function that prints a name.
  * @name: input name
@@ -8,7 +8,7 @@
 
 void print_name(char *name, void(*f)(char *))
 {
-	if (f)
+	if (f && name)
 		f(name);
 }
 
